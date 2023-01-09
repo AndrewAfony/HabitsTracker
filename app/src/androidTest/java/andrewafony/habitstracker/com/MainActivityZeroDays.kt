03ccd71fab893dd39c49dc880806fa3789b1e904
Andrew <andrew.afony@yandex.ru>
2023-01-09 18:44:12 +0300

    ui tests refactoring

--- app/src/androidTest/java/andrewafony/habitstracker/com/MainActivityZeroDays.kt
+++ app/src/androidTest/java/andrewafony/habitstracker/com/MainActivityZeroDays.kt
@@ -1,37 +1,22 @@
 package andrewafony.habitstracker.com
 
-import android.content.Context
-import android.content.Intent
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import android.content.SharedPreferences
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.CoreMatchers.not
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class MainActivityZeroDays {
+class MainActivityZeroDays : AbstractUiTest() {
 
-    @get:Rule
-    val activityScenario = lazyActivityScenarioRule<MainActivity>(launchActivity = false)
-
-    @Before
-    fun setup() {
-        val context = ApplicationProvider.getApplicationContext<App>()
-        val preferences = PreferencesProvider.Debug().create(context)
+    override fun init(preferences: SharedPreferences) {
         preferences.edit().clear().apply()
-        activityScenario.launch(Intent(context, MainActivity::class.java))
     }
 
     @Test
     fun test_zero_days_and_reset() {
-        onView(withId(R.id.daysNumber)).check(matches(ViewMatchers.withText("0")))
-        onView(withId(R.id.buttonReset)).check(matches(not(isDisplayed())))
+        MainPage().run {
+            daysText.check("0")
+            buttonReset.checkNotDisplayed()
+        }
     }
 }
