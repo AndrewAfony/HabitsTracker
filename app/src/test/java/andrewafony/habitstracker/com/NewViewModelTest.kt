49de50f971b8611a4ee8f1a42afc81f13292dda1
Andrew <andrew.afony@yandex.ru>
2023-01-11 12:20:31 +0300

    new view model tests

--- app/src/test/java/andrewafony/habitstracker/com/NewViewModelTest.kt
+++ app/src/test/java/andrewafony/habitstracker/com/NewViewModelTest.kt
@@ -27,7 +27,7 @@ class NewViewModelTest {
         assertEquals(1, communication.list.size)
 
         viewModel.addCard(position = 0)
-        assertEquals(NewUiState.Replace(position = 0, Card.Make), communication.list[1])
+        assertEquals(NewUiState.Replace(position = 0, card = Card.Make), communication.list[1])
         assertEquals(2, communication.list.size)
     }
 
@@ -41,11 +41,11 @@ class NewViewModelTest {
         assertEquals(1, communication.list.size)
 
         viewModel.addCard(position = 0)
-        assertEquals(NewUiState.Replace(position = 0, Card.Make), communication.list[1])
+        assertEquals(NewUiState.Replace(position = 0, card = Card.Make), communication.list[1])
         assertEquals(2, communication.list.size)
 
         viewModel.cancelCardCreation(position = 0)
-        assertEquals(NewUiState.Replace(position = 0, Card.Add), communication.list[2])
+        assertEquals(NewUiState.Replace(position = 0, card = Card.Add), communication.list[2])
         assertEquals(3, communication.list.size)
     }
 
@@ -59,14 +59,14 @@ class NewViewModelTest {
         assertEquals(1, communication.list.size)
 
         viewModel.addCard(position = 0)
-        assertEquals(NewUiState.Replace(position = 0, Card.Make), communication.list[1])
+        assertEquals(NewUiState.Replace(position = 0, card = Card.Make), communication.list[1])
         assertEquals(2, communication.list.size)
 
         interactor.canAddNewCard = true
         viewModel.saveCardCreation(text = "days without smoking", position = 0)
         assertEquals("days without smoking", interactor.saveNewCardList[0])
         assertEquals(1, interactor.saveNewCardList.size)
-        assertEquals(NewUiState.Replace(position = 0, Card.ZeroDays("days without smoking")), communication.list[2])
+        assertEquals(NewUiState.Replace(position = 0, card = Card.ZeroDays("days without smoking")), communication.list[2])
         assertEquals(NewUiState.Add(Card.Add), communication.list[3])
         assertEquals(4, communication.list.size)
         assertEquals(1, interactor.canAddNewCardList.size)
@@ -83,17 +83,32 @@ class NewViewModelTest {
         assertEquals(1, communication.list.size)
 
         viewModel.addCard(position = 0)
-        assertEquals(NewUiState.Replace(position = 0, Card.Make), communication.list[1])
+        assertEquals(NewUiState.Replace(position = 0, card = Card.Make), communication.list[1])
         assertEquals(2, communication.list.size)
 
         interactor.canAddNewCard = false
         viewModel.saveCardCreation(text = "days without smoking", position = 0)
         assertEquals("days without smoking", interactor.saveNewCardList[0])
         assertEquals(1, interactor.saveNewCardList.size)
-        assertEquals(NewUiState.Replace(position = 0, Card.ZeroDays("days without smoking")), communication.list[2])
+        assertEquals(NewUiState.Replace(position = 0, card = Card.ZeroDays("days without smoking")), communication.list[2])
         assertEquals(3, communication.list.size)
         assertEquals(1, interactor.canAddNewCardList.size)
         assertEquals(true, interactor.canAddNewCardList[0])
+    }
+
+    @Test
+    fun test_edit_zero_days_card_and_cancel() {
+        val communication = FakeCommunication()
+        val interactor = FakeInteractor(listOf(Card.ZeroDays(text = "days without smoking"), Card.Add))
+        val viewModel = NewViewModel(communication, interactor)
+        viewModel.init(isFirstRun = true)
+
+        assertEquals(NewUiState.Add(Card.ZeroDays(text = "days without smoking"), Card.Add), communication.list[0])
+        assertEquals(1, communication.list.size)
+
+        viewModel.editZeroDaysCard(position = 0)
+        assertEquals(NewUiState.Replace(position = 0, card = Card.ZeroDays("days without smoking")), 1)
+
     }
 }
 
